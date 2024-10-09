01bf0fc457f9e63ef767b4a3aac948c17073fc3d
André Felipe Schulle <andrefelipeschulle@icloud.com>
2024-10-09 15:35:44 -0300

    feat(build): create build script

---
+++ .changeset/five-peaches-shout.md
@@ -0,0 +1,5 @@
+---
+"@brainylab/resolver-validators": patch
+---
+
+create script from package.json on build
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts', 'src/typebox.ts', '!./src/**/*.spec.ts'],
+	entry: ['!./src/**/*.spec.ts'],
 	clean: true,
 	target: 'es2020',
 	format: ['cjs', 'esm'],
